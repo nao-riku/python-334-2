fe084f978b7b6e708661953c4f35ac919d05060c
nao-riku <76162238+nao-riku@users.noreply.github.com>
2022-09-16 21:27:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,18 @@ RETRY = 0
 
 def browser():
     print("Start Browsing")
-    print((os.environ.get('URL')))
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=options)
+    driver.set_window_size(620, 1)
+    driver.get(os.environ.get('URL')) 
+    time.sleep(3)
+    driver.quit()
+    print("End")
     
 now = datetime.datetime.today()
 if 27 < now.minute < 30:
